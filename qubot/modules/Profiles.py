dd7a2437b940441001e1bbac8da1b7a07e260049
martin-r-georgiev <martin.r.georgiev@hotmail.com>
2020-08-27 11:13:12 +0300

    Slight changes to profile module

--- qubot/modules/Profiles.py
+++ qubot/modules/Profiles.py
@@ -111,95 +111,96 @@ class Profiles(commands.Cog):
     @commands.command(name='profile', help=main.lang["command_profile_help"], description=main.lang["command_profile_description"], usage="", aliases=['p', 'me', 'level'])
     @commands.guild_only()
     async def profile(self, ctx, *, user: discord.User = None):
-        user = user or ctx.author
-        user_data = await ProfilesHandler.get(user.id, ctx.guild.id)
-        lang = main.get_lang(ctx.guild.id) if ctx.guild else main.lang
+        async with ctx.channel.typing():
+            user = user or ctx.author
+            user_data = await ProfilesHandler.get(user.id, ctx.guild.id)
+            lang = main.get_lang(ctx.guild.id) if ctx.guild else main.lang
 
-        image = self.background_image.copy().convert("RGBA")
-        draw = ImageDraw.Draw(image)
+            image = self.background_image.copy().convert("RGBA")
+            draw = ImageDraw.Draw(image)
 
-        # Custom background
-        if user_data['background'] != None:
-            custom_background = Image.open(os.path.join(main.bot_path, 'data', 'images', f'banner-{user_data["background"]}.jpg')).convert("RGBA")
-            overlay = Image.new('RGBA', custom_background.size, (0, 0, 0, 100))
-            custom_background = Image.alpha_composite(custom_background, overlay)
-            image.paste(custom_background, (0, 0))
+            # Custom background
+            if user_data['background'] != None:
+                custom_background = Image.open(os.path.join(main.bot_path, 'data', 'images', f'banner-{user_data["background"]}.jpg')).convert("RGBA")
+                overlay = Image.new('RGBA', custom_background.size, (0, 0, 0, 100))
+                custom_background = Image.alpha_composite(custom_background, overlay)
+                image.paste(custom_background, (0, 0))
 
-        user_title = (user.name[:25] + '..') if len(user.name) > 25 else user.name
-        draw.text((175, 50), user_title, fill=(255,255,255,255), font=self.title_font)
-        draw.text((180 + self.title_font.getsize(user_title)[0], 55), f'#{user.discriminator}', fill=(210, 210, 210, 255), font=self.medium_font)
-        draw.text((10, 185), lang["profile_bio_title"], fill=(255,255,255,255), font=self.title_font)
+            user_title = (user.name[:25] + '..') if len(user.name) > 25 else user.name
+            draw.text((175, 50), user_title, fill=(255,255,255,255), font=self.title_font)
+            draw.text((180 + self.title_font.getsize(user_title)[0], 55), f'#{user.discriminator}', fill=(210, 210, 210, 255), font=self.medium_font)
+            draw.text((10, 185), lang["profile_bio_title"], fill=(255,255,255,255), font=self.title_font)
 
-        offset = 220
-        user_bio = user_data['bio'] if user_data['bio'] else lang["profile_empty_bio"]
+            offset = 220
+            user_bio = user_data['bio'] if user_data['bio'] else lang["profile_empty_bio"]
 
-        for line in textwrap.wrap(user_bio, width=62):
-            draw.text((10, offset), line, font=self.bio_font, fill=(255,255,255,255))
-            offset += self.bio_font.getsize(line)[1]
+            for line in textwrap.wrap(user_bio, width=62):
+                draw.text((10, offset), line, font=self.bio_font, fill=(255,255,255,255))
+                offset += self.bio_font.getsize(line)[1]
 
-        # Leveling
-        user_experience = user_data['experience']
-        user_level = user_data['level']
-        user_next_lvl_experience = int(math.pow((user_level+1)/self.leveling_constant, 2))
+            # Leveling
+            user_experience = user_data['experience']
+            user_level = user_data['level']
+            user_next_lvl_experience = int(math.pow((user_level+1)/self.leveling_constant, 2))
 
-        levelbar_image = Image.new('RGBA', (self.levelbar_size[0] * 3, self.levelbar_size[1] * 3), (0,0,0,0))
-        levelbar_draw = ImageDraw.Draw(levelbar_image)
+            levelbar_image = Image.new('RGBA', (self.levelbar_size[0] * 3, self.levelbar_size[1] * 3), (0,0,0,0))
+            levelbar_draw = ImageDraw.Draw(levelbar_image)
 
-        levelbar_draw.rounded_rectangle([(0, 0), (self.levelbar_size[0] * 3, self.levelbar_size[1] * 3),], corner_radius=45, fill=(255, 255, 255, 255))
-        if (user_experience/user_next_lvl_experience) >= 0.06 :
-            levelbar_draw.rounded_rectangle([(6, 6), ((int(self.levelbar_size[0]*(user_experience/user_next_lvl_experience)) * 3) - 6, (self.levelbar_size[1]* 3) - 6),], corner_radius=39, fill=(115, 139, 215, 255))
-        else:
-            levelbar_draw.rounded_rectangle([(6, 6), ((int(self.levelbar_size[0]*(0.065)) * 3) - 6, (self.levelbar_size[1]* 3) - 6),], corner_radius=35, fill=(115, 139, 215, 255))
+            levelbar_draw.rounded_rectangle([(0, 0), (self.levelbar_size[0] * 3, self.levelbar_size[1] * 3),], corner_radius=45, fill=(255, 255, 255, 255))
+            if (user_experience/user_next_lvl_experience) >= 0.06 :
+                levelbar_draw.rounded_rectangle([(6, 6), ((int(self.levelbar_size[0]*(user_experience/user_next_lvl_experience)) * 3) - 6, (self.levelbar_size[1]* 3) - 6),], corner_radius=39, fill=(115, 139, 215, 255))
+            else:
+                levelbar_draw.rounded_rectangle([(6, 6), ((int(self.levelbar_size[0]*(0.065)) * 3) - 6, (self.levelbar_size[1]* 3) - 6),], corner_radius=35, fill=(115, 139, 215, 255))
 
-        levelbar_image = levelbar_image.resize((self.levelbar_size[0], self.levelbar_size[1]), Image.ANTIALIAS)
-        image.paste(levelbar_image, (175, 92), levelbar_image)
+            levelbar_image = levelbar_image.resize((self.levelbar_size[0], self.levelbar_size[1]), Image.ANTIALIAS)
+            image.paste(levelbar_image, (175, 92), levelbar_image)
 
-        disabled_leveling = LevelingToggle.is_disabled(ctx.guild.id)
+            disabled_leveling = LevelingToggle.is_disabled(ctx.guild.id)
 
-        xp_message = f'{user_experience}/{user_next_lvl_experience} {lang["exp_string"]}' if not disabled_leveling else lang["profile_disabled_exp"]
-        xp_w, xp_h = self.body_font.getsize(xp_message)
-        draw.text(((175 + (self.levelbar_size[0]-xp_w)/2), (90 + (self.levelbar_size[1]-xp_h)/2)), xp_message, fill=(0, 0, 0, 255), font=self.body_font)
+            xp_message = f'{user_experience}/{user_next_lvl_experience} {lang["exp_string"]}' if not disabled_leveling else lang["profile_disabled_exp"]
+            xp_w, xp_h = self.body_font.getsize(xp_message)
+            draw.text(((175 + (self.levelbar_size[0]-xp_w)/2), (90 + (self.levelbar_size[1]-xp_h)/2)), xp_message, fill=(0, 0, 0, 255), font=self.body_font)
 
-        if not disabled_leveling: 
-            level_message = f'{lang["level_string"]}: {user_level}'
-            user_rank = await ProfilesHandler.get_rank(user.id, ctx.guild.id)
-            rank_message = f'{lang["profile_rank"]}: {user_rank if user_rank else ctx.guild.member_count}'
-            
-            draw.text((175, 125), level_message, fill=(255, 255, 255, 255), font=self.medium_font)
-            draw.text((175 + self.levelbar_size[0] - self.medium_font.getsize(rank_message)[0], 125), rank_message, fill=(255, 255, 255, 255), font=self.medium_font)
+            if not disabled_leveling: 
+                level_message = f'{lang["level_string"]}: {user_level}'
+                user_rank = await ProfilesHandler.get_rank(user.id, ctx.guild.id)
+                rank_message = f'{lang["profile_rank"]}: {user_rank if user_rank else ctx.guild.member_count}'
+                
+                draw.text((175, 125), level_message, fill=(255, 255, 255, 255), font=self.medium_font)
+                draw.text((175 + self.levelbar_size[0] - self.medium_font.getsize(rank_message)[0], 125), rank_message, fill=(255, 255, 255, 255), font=self.medium_font)
 
-        # Avatar background
-        avatarbg_size = self.avatar_size + 12
-        avatarbg_image = Image.new('RGBA', (avatarbg_size, avatarbg_size), (86, 86, 86, 100))
-        avatar_bigsize = (avatarbg_size * 3, avatarbg_size * 3)
-        avatarbg_mask = Image.new('L', avatar_bigsize, 0)
-        avatarbg_draw = ImageDraw.Draw(avatarbg_mask) 
-        avatarbg_draw.ellipse((0, 0) + avatar_bigsize, fill=100)
-        avatarbg_mask = avatarbg_mask.resize(avatarbg_image.size, Image.ANTIALIAS)
+            # Avatar background
+            avatarbg_size = self.avatar_size + 12
+            avatarbg_image = Image.new('RGBA', (avatarbg_size, avatarbg_size), (86, 86, 86, 100))
+            avatar_bigsize = (avatarbg_size * 3, avatarbg_size * 3)
+            avatarbg_mask = Image.new('L', avatar_bigsize, 0)
+            avatarbg_draw = ImageDraw.Draw(avatarbg_mask) 
+            avatarbg_draw.ellipse((0, 0) + avatar_bigsize, fill=100)
+            avatarbg_mask = avatarbg_mask.resize(avatarbg_image.size, Image.ANTIALIAS)
 
-        image.paste(avatarbg_image, (14, 14), mask=avatarbg_mask)
+            image.paste(avatarbg_image, (14, 14), mask=avatarbg_mask)
 
-        # Avatar
-        avatar_asset = user.avatar_url_as(format='jpg', size=self.avatar_size)
-        buffer_avatar = io.BytesIO(await avatar_asset.read())
-        avatar_image = Image.open(buffer_avatar)
+            # Avatar
+            avatar_asset = user.avatar_url_as(format='jpg', size=self.avatar_size)
+            buffer_avatar = io.BytesIO(await avatar_asset.read())
+            avatar_image = Image.open(buffer_avatar)
 
-        # Masks
-        bigsize = (avatar_image.size[0] * 3, avatar_image.size[1] * 3)
-        mask = Image.new('L', bigsize, 0)
-        draw = ImageDraw.Draw(mask) 
-        draw.ellipse((0, 0) + bigsize, fill=255)
-        mask = mask.resize(avatar_image.size, Image.ANTIALIAS)
-        avatar_image.putalpha(mask)
+            # Masks
+            bigsize = (avatar_image.size[0] * 3, avatar_image.size[1] * 3)
+            mask = Image.new('L', bigsize, 0)
+            draw = ImageDraw.Draw(mask) 
+            draw.ellipse((0, 0) + bigsize, fill=255)
+            mask = mask.resize(avatar_image.size, Image.ANTIALIAS)
+            avatar_image.putalpha(mask)
 
-        image.paste(avatar_image, (20, 20), mask=mask)
+            image.paste(avatar_image, (20, 20), mask=mask)
 
-        # Sending image
-        buffer_output = io.BytesIO()                # Create buffer
-        image.save(buffer_output, format='PNG')
-        buffer_output.seek(0)
+            # Sending image
+            buffer_output = io.BytesIO()                # Create buffer
+            image.save(buffer_output, format='PNG')
+            buffer_output.seek(0)
 
-        await ctx.send(file=discord.File(buffer_output, 'profile.png'))
+            await ctx.send(file=discord.File(buffer_output, 'profile.png'))
 
     @commands.cooldown(3, 60, commands.BucketType.member)
     @commands.command(name='bio', help=main.lang["command_bio_help"], description=main.lang["command_bio_description"], usage="Some information about me...")
